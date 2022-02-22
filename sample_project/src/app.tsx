007ed5b7ef8a68485e264d464e9f6c96cb2c8406
ben <moon791017@gmail.com>
2022-02-22 09:20:33 +0800

    OK

--- sample_project/src/app.tsx
+++ sample_project/src/app.tsx
@@ -2,7 +2,14 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 function render() {
-  ReactDOM.render(<h2>Hello from React!</h2>, document.body);
+  ReactDOM.render(
+    <div>
+      <h1 className="text-3xl font-bold underline bg-green-900">
+        Hello world!
+      </h1>
+    </div>
+    ,
+    document.body);
 }
 
 render();
