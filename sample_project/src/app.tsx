b04dc5bf96e71bd848a1c95f6b04e7ffd3363b7b
ben <moon791017@gmail.com>
2022-02-22 09:31:06 +0800

    update

--- sample_project/src/app.tsx
+++ sample_project/src/app.tsx
@@ -1,15 +1,35 @@
 import * as React from 'react';
+import { useState } from 'react';
 import * as ReactDOM from 'react-dom';
 
-function render() {
-  ReactDOM.render(
+function Welcome(props: { name: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal; }) {
+  return <h1 className='bg-orange-500'>Hello, {props.name}</h1>;
+}
+function Example() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+
+  return (
     <div>
-      <h1 className="text-3xl font-bold underline bg-green-900">
-        Hello world!
-      </h1>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
     </div>
-    ,
-    document.body);
+  );
+}
+function App() {
+  return (
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+      <Example></Example>
+    </div>
+  );
 }
 
-render();
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
