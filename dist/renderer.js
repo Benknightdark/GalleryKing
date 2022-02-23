e18f421159bd55a8c058a5c0d3960ff24cb9a088
ben <moon791017@gmail.com>
2022-02-23 14:43:57 +0800

    aa

--- dist/renderer.js
+++
@@ -1,6 +0,0 @@
-var btn = document.querySelector(".mobile-menu-button");
-var sidebar = document.querySelector(".sidebar");
-btn.addEventListener("click", function () {
-    sidebar.style['display'] = sidebar.style['display'] === "inherit" ? "none" : "inherit";
-});
-//# sourceMappingURL=renderer.js.map
