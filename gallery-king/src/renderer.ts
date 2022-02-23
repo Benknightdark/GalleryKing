721fe276f868c06911cd008ab5a82d8dfbada4c4
ben <moon791017@gmail.com>
2022-02-23 14:10:30 +0800

    OK

--- gallery-king/src/renderer.ts
+++ gallery-king/src/renderer.ts
@@ -1,1 +1,5 @@
-console.log('renderer')
+const btn = document.querySelector(".mobile-menu-button");
+const sidebar = document.querySelector(".sidebar") as any;
+btn.addEventListener("click", () => {
+    sidebar.style['display'] = sidebar.style['display']==="inherit"?"none":"inherit"
+});
